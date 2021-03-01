ca9c60fc967f9a3d151c7faa5b819490d831359c
ARIJELAYNI <arij.elayni@gmail.com>
2021-03-01 20:27:47 +0100

    post form data

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react'
 
  class PostForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            body: ''
+        }
+
+    // binding functions
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+
+    }
+
+    onChange(e){
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    onSubmit(e){
+        e.preventDefault()
+        const newPost  = {
+            title: this.state.title,
+            body: this.state.body
+        }
+        fetch('http://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers:{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newPost)
+        }).then(res => res.json()).then(data => console.log(data))
+    }
     render() {
         return (
             <div>
 
-                <h1>Post Form</h1>
-                <form>
+                <h1>Add post</h1>
+                <form onSubmit={this.onSubmit}>
                     <div>
                         <label> Title: </label><br/>
-                        <input type="text" name="title" />
+                        <input type="text" name="title" value={this.state.title} onChange={this.onChange}/>
                     </div>
                     <div>
                         <label> Body: </label><br/>
-                        <textarea name="body" />
+                        <textarea name="body" value={this.state.body}  onChange={this.onChange}/>
                     </div>
                     <br />
                     <button type="submit"> Submit Me</button>
