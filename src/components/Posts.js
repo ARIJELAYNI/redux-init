58eddff79086d8509232f35df555d70c489ea387
ARIJELAYNI <arij.elayni@gmail.com>
2021-03-01 19:46:37 +0100

    posts as a component state value

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react'
 
 import axios from 'axios'
 class Posts extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state={
+            posts: []
+        }
+    }
+
     componentDidMount() {
         axios.get('http://jsonplaceholder.typicode.com/posts')
-        .then(res =>  console.log(res.data))
+        .then(res =>  this.setState({posts: res.data}))
     }
+
+
     render() {
+        const postItems = this.state.posts.map(post => (
+            <div key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+        ))
         return (
             <div>
                 <h1>Posts</h1>
+                <div>{postItems}</div>
             </div>
         )
     }
